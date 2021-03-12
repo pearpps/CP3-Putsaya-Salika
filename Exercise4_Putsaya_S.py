66f8cc1b790aed0c2677f2dfbd41cd2af533467b
pearpps <pearr.putsaya@gmail.com>
2021-03-12 15:07:12 +0700

    Added Exercise4

---
+++ Exercise4_Putsaya_S.py
@@ -0,0 +1,25 @@
+'''
+Your Score in Foundation English
+'''
+FE = 80
+
+'''
+Your Score in General Business
+'''
+GB = 75
+
+'''
+Your Score in Introduction to Computer Systems
+'''
+ICS = 70
+
+'''
+Your Score in Computer Programming
+'''
+CP = 75
+
+print("----------------Your Score---------------")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming :",CP)
