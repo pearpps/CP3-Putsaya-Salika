9ce10d67e183b09cac8d0d000533369ac16ba2bd
pearpps <pearr.putsaya@gmail.com>
2021-03-15 19:20:46 +0700

    Added Lecture50

---
+++ Lecture50_Putsaya_S.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multipleNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+addNumber(100,250)
+minusNumber(100,250)
+multipleNumber(100,250)
+divideNumber(100,250)
