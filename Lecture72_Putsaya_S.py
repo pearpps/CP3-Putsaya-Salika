0cb9ee091d7216d58085e6a98591f9dfe424fb2f
pearpps <pearr.putsaya@gmail.com>
2021-03-17 19:57:31 +0700

    Added Lecture72

---
+++ Lecture72_Putsaya_S.py
@@ -0,0 +1,23 @@
+menuList = []
+sum = 0
+
+def showBill():
+    print("----- MY FOOD -----")
+    for number in range(len(menuList)):
+        print(menuList[number][0],menuList[number][1])
+    print("-------------------")
+    print("Total Price =",sum)
+    print("-----Thank You-----")
+
+while True:
+    print("If you're finished Please type EXIT")
+    menuName = input("Please enter menu : ")
+    if menuName.lower() == "exit" :
+        break
+    else:
+        menuPrice = int(input("Price : "))
+        sum += menuPrice
+        menuList.append([menuName,menuPrice])
+
+showBill()
+
